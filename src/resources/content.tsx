7269501c058470866b0c83954c953d77b5ee32a3
jsged <jsgedwards@outlook.com>
2025-11-16 14:56:38 +0000

    deleted example desc.

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -102,9 +102,7 @@ const about: About = {
     title: "Introduction",
     description: (
       <>
-        Selene is a Jakarta-based design engineer with a passion for transforming complex challenges
-        into simple, elegant design solutions. Her work spans digital interfaces, interactive
-        experiences, and the convergence of design and technology.
+        
       </>
     ),
   },
