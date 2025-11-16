d2f2bc2f5ab62cb3ae9adaddaee4969deeeca91e
jsged <jsgedwards@outlook.com>
2025-11-16 14:54:33 +0000

    Added work experience

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -113,41 +113,46 @@ const about: About = {
     title: "Work Experience",
     experiences: [
       {
-        company: "FLY",
-        timeframe: "2022 - Present",
-        role: "Senior Design Engineer",
+        company: "Aurian Lighting",
+        timeframe: "November 2025",
+        role: "Set Construction, Rigging and Followspot Op.",
         achievements: [
           <>
-            Redesigned the UI/UX for the FLY platform, resulting in a 20% increase in user
-            engagement and 30% faster load times.
+            Set Constrcution role was completed with my dad, Paul Edwards on behalf of <i>Salt & Pepper Productions</i>.
           </>,
           <>
-            Spearheaded the integration of AI tools into design workflows, enabling designers to
-            iterate 50% faster.
+            Rigging and Followspot Op. was completed on behalf of Aurian Lighting at The Grange Theatre, Northwich.
+          </>,
+          <>
+            Learnt the manual operation of the Chauvet Followspot 120S.
           </>,
         ],
         images: [
           // optional: leave the array empty if you don't want to display images
           {
-            src: "/images/projects/project-01/cover-01.jpg",
-            alt: "Once UI Project",
+            src: "/images/projects/gbbo/cover.jpeg",
+            alt: "Great British Bake Off: The Musical @ The Grange Theatre, Northwich",
             width: 16,
             height: 9,
           },
         ],
       },
       {
-        company: "Creativ3",
-        timeframe: "2018 - 2022",
-        role: "Lead Designer",
+        company: "Bailey Audio Systems",
+        timeframe: "July 2025",
+        role: "Work Experience Technician",
         achievements: [
           <>
-            Developed a design system that unified the brand across multiple platforms, improving
-            design consistency by 40%.
+            Completed and Passed my Year 10 (High School) work experience.
           </>,
           <>
-            Led a cross-functional team to launch a new product line, contributing to a 15% increase
-            in overall company revenue.
+            Learnt how to use the FLX S24 (ZerOS) from Zero 88.
+          </>,
+          <>
+            Did lighting desing on operation for Frozen Jr. (The Muscial) at Rudheath High School.
+          </>,
+          <>
+            Images Soon to Come.
           </>,
         ],
         images: [],
